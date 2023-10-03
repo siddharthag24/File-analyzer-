eeb18efdfe1f107bbab7ca78c30608d7dca1f124
Alokik24 <137095971+Alokik24@users.noreply.github.com>
2023-10-04 00:12:50 +0530

    Add files via upload

---
+++ file.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class User
+{
+    // User attributes and methods
+private:
+    string username;
+    string password;
+
+public:
+    // Constructor
+    User(const string &username, const string &password);
+
+    string getUsername() const;
+    string getPassword() const;
+
+    void setPassword(const string &newPassword);
+};
+
+class UserManager
+{
+private:
+    unordered_map<string, User> userDatabase;
+
+public:
+    bool addUser(const User &user);
+    bool removeUser(const string &username);
+    User *getUser(const string &username);
+    bool authenticateUser(const string &username, const string &password);
+};
+
+User::User(const string &username, const string &password)
+{
+    this->username = username;
+    this->password = password;
+}
+
+string User::getUsername() const
+{
+    return username;
+}
+string User::getPassword() const
+{
+    return password;
+}
+void User::setPassword(const string &newPassword)
+{
+    password = newPassword;
+}
+
+bool UserManager ::addUser(const User &user)
+{
+    auto result = userDatabase.emplace(user.getUsername(), user);
+    return result.second; // Indicates whether the user was added successfully
+}
+bool UserManager ::removeUser(const string &username)
+{
+    return userDatabase.erase(username) > 0;
+}
+User *UserManager ::getUser(const string &username)
+{
+    auto it = userDatabase.find(username);
+    if (it != userDatabase.end())
+    {
+        return &(it->second);
+    }
+    return NULL;
+}
+bool UserManager ::authenticateUser(const string &username, const string &password)
+{
+    auto it = userDatabase.find(username);
+    if (it != userDatabase.end())
+    {
+        return it->second.getPassword() == password;
+    }
+    return false;
+}
+
+class UserAuthentication
+{
+    // User authentication logic
+};
+
+class File
+{
+    // File attributes and methods
+};
+
+class Folder
+{
+    // Folder attributes and methods
+};
+
+class Directory
+{
+    // Directory attributes and methods
+};
+
+class Analyzer
+{
+    // File analysis methods
+};
+
+class FileManager
+{
+    // File utility functions
+};
+
+int main()
+{
+    // Create a UserManager instance
+    UserManager userManager;
+
+    // Add some users
+    User user1("user1", "password1");
+    User user2("user2", "password2");
+    User user3("user3", "password3");
+
+    if (userManager.addUser(user1))
+    {
+        cout << "User1 added successfully." << endl;
+    }
+    if (userManager.addUser(user2))
+    {
+        cout << "User2 added successfully." << endl;
+    }
+    if (userManager.addUser(user3))
+    {
+        cout << "User3 added successfully." << endl;
+    }
+
+    // Attempt to authenticate a user
+    if (userManager.authenticateUser("user1", "password1"))
+    {
+        cout << "User1 authenticated successfully." << endl;
+    }
+    else
+    {
+        cout << "User1 authentication failed." << endl;
+    }
+
+    // Retrieve and display user information
+    User *user = userManager.getUser("user2");
+    if (user)
+    {
+        cout << "User2 details:" << endl;
+        cout << "Username: " << user->getUsername() << endl;
+        cout << "Password: " << user->getPassword() << endl;
+    }
+    else
+    {
+        cout << "User2 not found." << endl;
+    }
+
+    // Remove a user
+    if (userManager.removeUser("user3"))
+    {
+        cout << "User3 removed successfully." << endl;
+    }
+    else
+    {
+        cout << "User3 removal failed." << endl;
+    }
+
+    return 0;
+}
